535041814e2922c6fc86cfe84fa9750028f36604
Carlos Rios <c.riosp@uniandes.edu.co>
2026-03-02 15:14:36 -0500

    update de ip's de aws

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.167.253.26"]
+# debe ir IP publica del visualizador 
+ALLOWED_HOSTS = ["localhost", "98.80.247.90"]
 
 
 # Application definition
@@ -96,7 +97,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "18.234.151.152",  # Dirección IP de la base de datos
+        "HOST": "54.161.122.243",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +157,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.173.74.46"
+MQTT_HOST = "98.87.165.238"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
