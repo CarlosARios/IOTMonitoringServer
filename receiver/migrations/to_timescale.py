41c85c83eddf9d01e6882534e735fca34b776868
Carlos Rios <c.riosp@uniandes.edu.co>
2026-03-01 20:04:01 -0500

    update proyecto

--- receiver/migrations/to_timescale.py
+++ receiver/migrations/to_timescale.py
@@ -10,9 +10,13 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        # Habilita la extensión de TimescaleDB si está instalada en PostgreSQL.
+        migrations.RunSQL(
+            "CREATE EXTENSION IF NOT EXISTS timescaledb;"
+        ),
         # Crea la hipertabla de timescale con chunks de 3 días.
         migrations.RunSQL(
-            "SELECT create_hypertable('\"receiver_data\"', 'time', chunk_time_interval=>259200000000);"
+            "SELECT create_hypertable('\"receiver_data\"', 'time', chunk_time_interval => INTERVAL '3 days', if_not_exists => TRUE);"
         ),
         # Configura la compresión para estaciones y variables. Son llaves foráneas de la tabla principal.
         migrations.RunSQL(
@@ -22,6 +26,6 @@ class Migration(migrations.Migration):
         ),
         # Comprime los datos cada 7 días.
         migrations.RunSQL(
-            "SELECT add_compression_policy('\"receiver_data\"', 604800000000);"
+            "SELECT add_compression_policy('\"receiver_data\"', INTERVAL '7 days');"
         ),
     ]
